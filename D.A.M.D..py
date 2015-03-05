f472a39a47177d3d31c7e0eb8e32def61cf9ebc9
unknown <mannd265@CE105WD554921S.hillnet.ad.hsd.k12.or.us>
2015-03-05 15:15:39 -0800

    Template

---
+++ D.A.M.D..py
@@ -0,0 +1,35 @@
+#D.A.M.D. Text Based Adventure
+#BY:
+#   Damian Manning-Henseler
+#   Andrew Roundtree
+#   Micah Belden
+#   Deryk Patraw
+
+from tkinter import *
+import tkinter.simpledialog
+import tkinter.messagebox
+
+root = Tk()
+w = Label(root, text="Choose Your Own Adventure")
+w.pack()
+
+###Intro###
+def intro():
+    messagebox.showinfo("","")
+
+
+###Damian's Functions###
+
+
+
+###Andrew's Functions###
+
+
+
+###Micah's Functions###
+
+
+
+###Deryk's Functions###
+
+
