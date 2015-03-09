0bff174aa3d4c7a28ffb2bf8d649e6a6bfcf88b5
unknown <damianmh392@hotmail.com>
2015-03-08 20:20:19 -0700

    Intro?

--- D.A.M.D..py
+++ D.A.M.D..py
@@ -10,16 +10,34 @@ import tkinter.simpledialog
 import tkinter.messagebox
 
 root = Tk()
-w = Label(root, text="Choose Your Own Adventure")
+w = Label(root, text="A D.A.M.D. Adventure")
 w.pack()
 
 ###Intro###
 def intro():
-    messagebox.showinfo("","")
+    messagebox.showinfo("The Crossroads","You wake up in a four way intersection. there are small dirt" +
+                        " paths in a field, one going North, one South, one East, and one West. Which" +
+                        " path do you wish to choose?")
+    path = simpledialog.askstring("Where to Go","Type ""North"" to go north and so on to choose your path.")
+    if path == "North":
+        north()
+
+    elif path == "South":
+        south()
+
+    elif path == "East":
+        east()
+
+    elif path == "West":
+        west()
+
+    else:
+        intro()    
 
 
 ###Damian's Functions###
-
+def south():
+    messagebox.showinfo("South","")
 
 
 ###Andrew's Functions###
@@ -31,5 +49,13 @@ def intro():
 
 
 ###Deryk's Functions###
+
+
+
+### Main ###
+intro()
+
+root.destroy()        
+        
 
 
